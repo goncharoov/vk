54664165830ef4fdb9da3659b59ad4039e0268b6
goncharoov <takeyo69@icloud.com>
2020-11-30 21:01:19 +1000

    modified PostManager.java

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -1,4 +1,19 @@
 package ru.netology.manager;
 
+import ru.netology.domain.Post;
+
 public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return posts;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public Post[] delete(int ownerId, int postId) {
+        return null;
+    }
 }
