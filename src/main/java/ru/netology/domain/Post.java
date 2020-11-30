255f4a1b39392a810181de3c918bc812a9218579
goncharoov <takeyo69@icloud.com>
2020-11-30 18:22:25 +1000

    added classes

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class Post {
+
+    private int id;
+    private int ownerId;
+    private int autorId;
+    private int dateOfPost;
+    private Actions actions;
+    private DataBlock dataBlock;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private ShareInfo shareInfo;
+    private int viewsCount;
+
+
+}
